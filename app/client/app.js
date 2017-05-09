670fad3224b536af87722b4b050dd536bad207d8
Fahid Mohammed <fahid.rabiu@hotmail.com>
2017-05-09 11:05:51 +0100

    Implemented authorization feature, created scaffold to allow for its configuration to match the requirement of the  App where the UMS is being used

--- app/client/app.js
+++ app/client/app.js
@@ -4,14 +4,43 @@
 
 
 angular.module('myApp', [
-
   'app.user',
   'app.role',
-  'config',
-  'ngRoute'
+  'app.config',
+  'ngRoute',
+  'ngCookies'
+])
+  .factory('authInterceptor', [ '$rootScope', '$q', '$cookies', '$window' ,function($rootScope, $q, $cookies, $window) {
+    return {
 
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/users/list'});
-}]);
+      //TODO modify to be more generic
+
+      request: function(config) {
+        config.headers = config.headers || {};
+        if ($cookies.get('token')) {
+          config.headers.Authorization = 'Bearer '.concat($cookies.getObject('token'));
+        } else {
+           $window.location.href = '/login';
+        }
+        return config;
+      },
+
+
+      responseError: function(response) {
+        if (response.status === 401) {
+          $rootScope.$emit('unauthorized');
+          $cookies.remove('token');
+          // $window.location.href = '/login';
+          return $q.reject(response);
+        } else {
+          return $q.reject(response);
+        }
+      }
+    };
+  }])
+
+  .config(['$locationProvider', '$routeProvider','$httpProvider', function($locationProvider, $routeProvider, $httpProvider) {
+    $locationProvider.hashPrefix('!');
+    $httpProvider.interceptors.push('authInterceptor');
+    $routeProvider.otherwise({redirectTo: '/users/list'});
+  }]);
---
+++ app/client/config/config.values.js
@@ -0,0 +1,15 @@
+'use strict';
+angular.module('app.config')
+  .constant('SETTINGS', {
+    userTypes: ['dashboard', 'mobile'],
+    auth: {
+      type: 'COOKIES',
+      cookies: {
+        field: 'token',
+        authType: 'bearer'
+      },
+      redirectUrl: '/'
+    }
+  })
+  .value('PAGE_SIZE', 10)
+;
