955836c030f16c4679c2e683e45e07a3ddb3825e
Fahid Mohammed <fahid.rabiu@hotmail.com>
2017-05-22 10:18:33 +0100

    removed some dependencies that are no longer required

--- app/client/user/users-dependency.service.js
+++ app/client/user/users-dependency.service.js
@@ -3,24 +3,16 @@
 'use strict';
 
 angular.module('app.user')
-  .service('usersDependencyService', ['$http', '$q', function ($http, $q) {
-
+  .service('usersDependencyService',  function () {
 
       this.methods = {}
 
-
       this.get = function (resource, param) {
-        //alert(resource);
         if (this.methods[resource]){
           return this.methods[resource](param);
         }
         else {
-          alert("qq");
           return null;
         }
-
       }
-
-
-
-  }]);
+  });
