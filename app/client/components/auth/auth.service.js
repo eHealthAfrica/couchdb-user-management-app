478641ebe5cfcafcfab46ecc2a82e4559b781529
Fahid Mohammed <fahid.rabiu@hotmail.com>
2017-08-01 10:35:57 +0100

    removed comments

--- app/client/components/auth/auth.service.js
+++ app/client/components/auth/auth.service.js
@@ -30,15 +30,13 @@ angular.module('app.auth', [])
       }
 
       if (SharedAuth.isLoggedIn()) {
-        /* var authorizationFieldPath = config.access.field.split(".");
+         var authorizationFieldPath = config.access.field.split(".");
          var authorizationRequirement = config.access.value;
          var userAuthorization = currentUser;
          for (var i in authorizationFieldPath) {
          userAuthorization =  userAuthorization[authorizationFieldPath[i]];
          }
          if (userAuthorization === authorizationRequirement) { return true;}
-         }*/
-        return true;
       }
       $window.location.href = config.auth.redirectUrl;
     }
