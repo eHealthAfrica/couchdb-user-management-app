b5ec4bee5e3c4e18e443f52f834b8571d812afd0
Musa <musakunte@gmail.com>
2017-08-11 11:28:31 +0100

    added  more ums config overrides

--- app/index.js
+++ app/index.js
@@ -5,9 +5,12 @@ const os = require('os')
 const config = require('../test/config')
 const app = require('./server/app')(config)
 const PORT = process.env.UMS_PORT || 3331
+let access = {}
+try { access = require('./access.json') } catch (e) { access = config.access }
 config.couch.db = process.env.UMS_DB || 'set'
 config.currentUser.url = process.env.UMS_USER_URL || 'http://localhost:3333/api/v1/users/me'
 config.auth.redirectUrl = process.env.UMS_REDIRECT_URL || 'http://localhost:3000'
+config.access = access
 
 http.createServer(app)
   .listen(PORT, () => {
