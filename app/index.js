001dd5e670209d31cd65a022bc88bf8887ecbd91
Musa <musakunte@gmail.com>
2017-08-14 09:56:11 +0100

    updated standalone index with config override

--- app/index.js
+++ app/index.js
@@ -2,14 +2,16 @@
 
 const http = require('http')
 const os = require('os')
-const config = require('../test/config')
+const testConfig = require('../test/config')
 const app = require('./server/app')(config)
 const PORT = process.env.UMS_PORT || 3331
+let config = try {require('./ums-config')} catch (e) {config = testConfig}
 let access = {}
 try { access = require('./access.json') } catch (e) { access = config.access }
-config.couch.db = process.env.UMS_DB || 'set'
-config.currentUser.url = process.env.UMS_USER_URL || 'http://localhost:3333/api/v1/users/me'
-config.auth.redirectUrl = process.env.UMS_REDIRECT_URL || 'http://localhost:3000'
+
+config.couch.db = process.env.UMS_DB || config.couch.db || 'set'
+config.currentUser.url = process.env.UMS_USER_URL || config.currentUser.url || 'http://localhost:3333/api/v1/users/me'
+config.auth.redirectUrl = process.env.UMS_REDIRECT_URL || config.auth.redirectUrl  || 'http://localhost:3000'
 config.access = access
 
 http.createServer(app)
